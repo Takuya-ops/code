e340b70147f32cb63480dbaace0ddd03d0136955
Takuya-ops <hora2112.ipu@gmail.com>
2023-11-18 10:18:04 +0900

    test case add

--- go/hsd/hsd_test.go
+++ go/hsd/hsd_test.go
@@ -24,11 +24,13 @@ func TestStringDistance(t *testing.T) {
 		rhs  string
 		want int
 	}{
-		// テストケースをまとめる(name:表示させるメッセージ、lhs&foo:関数の引数, want:期待値)
+		// テストケースをまとめる(name:テスト名、lhs&foo:関数の引数, want:期待値)
 		{name: "lhs is longer than rhs", lhs: "foot", rhs: "foo", want: -1},
 		{name: "rhs is shorter than rhs", lhs: "foo", rhs: "foot", want: -1},
 		{name: "diff 0", lhs: "foot", rhs: "foot", want: 0},
 		{name: "diff 1", lhs: "foot", rhs: "foat", want: 1},
+		{name: "diffs 2", lhs: "foots", rhs: "foatt", want: 2},
+		{name: "diffs 3", lhs: "footts", rhs: "faotat", want: 3},
 	}
 	for _, tc := range tests {
 		got := StringDistance(tc.lhs, tc.rhs)
